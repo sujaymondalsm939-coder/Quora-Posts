6b2c0051b0fac9d97e6fd75d808d5cfc859aebf7
sujay <sujaymondalsm939@gamil.com>
2026-09-01 20:42:51 +0530

    install method-override

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-// const methodOverride = require('method-override'); 
+const methodOverride = require('method-override'); 
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
